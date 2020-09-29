ed451b891436eef594a42c6abe32bcb4bba59594
Christian Barra <me@christianbarra.com>
2020-09-29 23:55:04 +0200

    Move navbar to center

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,35 +3,40 @@ import { Link } from "gatsby"
 
 const Navbar = () => {
   return (
-    <header className="max-w-5xl mx-auto pt-8 pb-5">
+    <header className="max-w-xl mx-auto pt-8 pb-5">
+    {/* <header className="sm:container sm:mx-auto lg:w-2/4 p-4"> */}
       <div className="flex flex-wrap -mx-2 overflow-hidden px-5 lg:px-0">
-        <div className="my-2 px-2 w-full overflow-hidden md:w-1/8 lg:w-1/8 xl:w-1/8 text-center md:text-left">
+        <div className="my-2 px-2 w-full overflow-hidden md:w-1/8 lg:w-1/8 xl:w-1/8 text-center">
           <h1 className="font-bold text-2xl">Christian Barra</h1>
         </div>
-        <nav className="my-2 px-2 w-full overflow-hidden md:w-1/8 lg:w-1/8 xl:w-1/8 text-center md:text-left">
+        <nav className="my-2 px-2 w-full overflow-hidden md:w-1/8 lg:w-1/8 xl:w-1/8 text-center">
           <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow">
               <Link
                 className="block text-base font-semibold mt-4 lg:inline-block lg:mt-0 hover:underline mr-4"
                 to="/"
+                alt="Home"
               >
                 Home
               </Link>
               <Link
                 className="block text-base font-semibold mt-4 lg:inline-block lg:mt-0 hover:underline mr-4"
                 to="/consulting-and-training"
+                alt="Consulting &amp; Training"
               >
                 Consulting &amp; Training
               </Link>
               <Link
                 className="block text-base font-semibold mt-4 lg:inline-block lg:mt-0 hover:underline mr-4"
                 to="/posts"
+                alt="Posts"
               >
-                Articles
+                Posts
               </Link>
               <Link
                 className="block text-base font-semibold mt-4 lg:inline-block lg:mt-0 hover:underline mr-4"
                 to="/speaking"
+                alt="Speaking"
               >
                 Speaking
               </Link>
